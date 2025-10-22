c58cbafffdbbe603fa14c4486e34a0e2c1134829
hideharu <hideharusawada9@gmail.com>
2025-10-22 22:18:57 +0900

    20251022

--- lib/nodemailer.ts
+++ lib/nodemailer.ts
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: true, // 465ならtrue
+  secure: false, // 465ならtrue
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
